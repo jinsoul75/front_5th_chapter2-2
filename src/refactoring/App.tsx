fa918f484b79d07f1e1c81f82b358e32570236e8
jinsoul <wlsthf75@zetalux.co.kr>
2025-04-24 09:57:49 +0900

    chore: 쿠폰, 상품 상수화

--- src/refactoring/App.tsx
+++ src/refactoring/App.tsx
@@ -1,55 +1,13 @@
 import { useState } from 'react';
 import { CartPage, AdminPage } from './pages';
-import { Coupon, Product } from '@/types';
 import { useCoupons, useProducts } from '@/features/cart/hooks';
 import { Navigation } from './components';
-import { NAVIGATION_TEXT } from './constants';
+import { COUPONS, NAVIGATION_TEXT, PRODUCTS } from './constants';
 
-const initialProducts: Product[] = [
-  {
-    id: 'p1',
-    name: '상품1',
-    price: 10000,
-    stock: 20,
-    discounts: [
-      { quantity: 10, rate: 0.1 },
-      { quantity: 20, rate: 0.2 },
-    ],
-  },
-  {
-    id: 'p2',
-    name: '상품2',
-    price: 20000,
-    stock: 20,
-    discounts: [{ quantity: 10, rate: 0.15 }],
-  },
-  {
-    id: 'p3',
-    name: '상품3',
-    price: 30000,
-    stock: 20,
-    discounts: [{ quantity: 10, rate: 0.2 }],
-  },
-];
-
-const initialCoupons: Coupon[] = [
-  {
-    name: '5000원 할인 쿠폰',
-    code: 'AMOUNT5000',
-    discountType: 'amount',
-    discountValue: 5000,
-  },
-  {
-    name: '10% 할인 쿠폰',
-    code: 'PERCENT10',
-    discountType: 'percentage',
-    discountValue: 10,
-  },
-];
 
 const App = () => {
-  const { products, updateProduct, addProduct } = useProducts(initialProducts);
-  const { coupons, addCoupon } = useCoupons(initialCoupons);
+  const { products, updateProduct, addProduct } = useProducts(PRODUCTS);
+  const { coupons, addCoupon } = useCoupons(COUPONS);
   const [isAdmin, setIsAdmin] = useState(false);
 
   return (
--- src/refactoring/constants/index.ts
+++ src/refactoring/constants/index.ts
@@ -1,2 +1,4 @@
 export * from './navigation';
 export * from './currency';
+export * from './coupon';
+export * from './products';
